e5c25b1b089b933a10f79ec79a3c75adcf5fb312
tomohikohorai-source <tomohiko.horai@gmail.com>
2026-01-15 19:21:02 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  // 重要：末尾にスラッシュを付けてください
-  site: 'https://penan-japanese-news.vercel.app/',
+  // あなたのURL。末尾のスラッシュはなしで設定してみましょう。
+  site: 'https://penan-japanese-news.vercel.app',
   integrations: [sitemap()],
   output: 'static',
 });
